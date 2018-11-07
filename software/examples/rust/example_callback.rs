2fe016b87ed5622a2749bb4e94789b6ee5b401be
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:25:18 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for state changed events.
-    let state_changed_receiver = db.get_state_changed_receiver();
+    let state_changed_receiver = db.get_state_changed_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `db` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `db` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for state_changed in state_changed_receiver {
