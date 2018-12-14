506d5fe31be433a1850019746edfd0567e75d748
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:08:44 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/dual_button_v2_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Dual Button Bricklet 2.0.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	db, _ := dual_button_v2_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	db.RegisterStateChangedCallback(func(buttonL dual_button_v2_bricklet.ButtonState, buttonR dual_button_v2_bricklet.ButtonState, ledL dual_button_v2_bricklet.LEDState, ledR dual_button_v2_bricklet.LEDState) {
+
+		if buttonL == dual_button_v2_bricklet.ButtonStatePressed {
+			fmt.Println("Left Button: Pressed")
+		} else if buttonL == dual_button_v2_bricklet.ButtonStateReleased {
+			fmt.Println("Left Button: Released")
+		}
+
+		if buttonR == dual_button_v2_bricklet.ButtonStatePressed {
+			fmt.Println("Right Button: Pressed")
+		} else if buttonR == dual_button_v2_bricklet.ButtonStateReleased {
+			fmt.Println("Right Button: Released")
+		}
+
+		fmt.Println()
+	})
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
